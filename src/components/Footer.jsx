e5aaa88e31ef8ee902bf96ee9e25b138d4da9ad6
Bina Duwal <duwalbina2@gmail.com>
2025-09-13 21:42:07 +0545

    feat:add contact page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section className="mx-auto container flex  justify-center flex-col items-center mt-20 dark:text-white">
-      <p>Designed and built with ❤️ by Bina Duwal</p>
+    <section className="flex  bg-gray-800 p-2 sm:p-4 justify-center flex-col items-center mt-20 dark:text-white">
+      <p >Designed and built with ❤️ by Bina Duwal</p>
+
     </section>
   );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,44 +1,65 @@
 import React from "react";
 import { SlEnvolopeLetter } from "react-icons/sl";
+import { FaMapLocationDot } from "react-icons/fa6";
+import SocialMediaIcons from "../components/SocialMediaIcons";
 
 const Contact = () => {
   return (
-    <section className="mx-auto container mt-20 dark:text-white p-2 ">
-    <div className="flex justify-between">
-
-    <div >
-      <h1 className="text-4xl font-bold text-center  ">
-        Let’s <span className="text-pink-700">Connect</span>
+    <section className="mx-auto container mt-20 dark:text-white mb-3">
+      <h1 className="text-4xl text-center font-bold mb-4 ">
+        Let's <span className="text-pink-700">Connect</span>
       </h1>
 
-      <div>
+      <div className="flex justify-between">
+<div className="">
+  <div className="flex items-center gap-4  p-4 rounded">
+    <div className="text-pink-700 text-3xl">
       <SlEnvolopeLetter />
-      </div>
-
     </div>
-      <div className="flex items-center justify-center">
-        <form className=" w-full max-w-lg   text-center"  >
-          <input
-            type="text"
-            placeholder="Enter your name"
-            className="w-full p-2 mt-5 border border-gray-300 rounded-md"
-          />
-          <input
-            type="email"
-            placeholder="Enter your email address"
-            className="w-full p-2 mt-5 border border-gray-300 rounded-md"
-          />
-          <input
-            type="text"
-            placeholder="Enter your message "
-            className="w-full p-2 mt-5 border border-gray-300 rounded-md"
-          />
-          <button className="bg-pink-700 text-white p-2 mt-5 rounded-md">
-            Submit
-          </button>
-        </form>
-            </div>
+    <div>
+      <p className="font-bold text-xl sm:text-2xl">Mail</p>
+      <p className="text-gray-400 text-base">duwalbina2@gmail.com</p>
+    </div>
+  </div>
 
+  <div className="flex items-center gap-4 p-4 rounded">
+    <div className="text-pink-700 text-3xl">
+      <FaMapLocationDot />
+    </div>
+    <div>
+      <p className="font-bold text-xl sm:text-2xl">Location</p>
+      <p className="text-gray-400 text-base">Jhaukhel, Bhaktapur</p>
+    </div>
+  </div>
+
+  <SocialMediaIcons/>
+</div>
+
+        <div className="flex items-center justify-center">
+          <form className=" w-full max-w-lg   text-center">
+            <input
+              type="text"
+              placeholder="Enter your name"
+              className="w-full p-2 mt-5 border border-gray-300 rounded-md"
+            />
+            <input
+              type="email"
+              placeholder="Enter your email address"
+              className="w-full p-2 mt-5 border border-gray-300 rounded-md"
+            />
+            <input
+              type="text"
+              placeholder="Enter your message "
+              className="w-full p-2 mt-5 border border-gray-300 rounded-md"
+            />
+            <div className="flex">
+
+            <button className="bg-pink-700  text-white p-2 mt-5 rounded-md">
+              Submit
+            </button>
+            </div>
+          </form>
+        </div>
       </div>
     </section>
   );
